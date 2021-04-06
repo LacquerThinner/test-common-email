d223ca97a6547f8b5d2f6fb6259045532d2b877c
Kyle Driver <driver8684@gmail.com>
2021-04-05 20:32:11 -0400

    Added constant variables, before and after methods, and addBcc() test cases

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -16,4 +16,40 @@ import org.junit.Test;
 
 public class EmailTest {
 
+	private static final String[] TEST_EMAILS = { "Test@Email.com", "Email@Test.com",
+	"TestEmail@EmailTest.org" };
+	
+	private static final String[] TEST_INVALID_EMAILS = { };
+	
+	/* Concrete Email Class for testing */
+	private EmailConcrete email;
+	
+	/*creates an email object*/
+	@Before
+	public void setUpEmailTest() throws Exception {
+		
+		email = new EmailConcrete();
+		
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception {
+		
+	}
+
+	/*Tests addBcc with a list of test emails*/
+	@Test
+	public void testAddBcc() throws Exception {
+		
+		email.addBcc(TEST_EMAILS);
+		
+		assertEquals(3, email.getBccAddresses().size());
+	}
+	/*Tests addBcc throws an email exception if a list of invalid emails is given*/
+	@Test(expected=EmailException.class)
+	public void testEmailExceptionAddBcc() throws Exception {
+		
+		email.addBcc(TEST_INVALID_EMAILS);
+		
+	}
 }
