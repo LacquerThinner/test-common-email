350fa58861ebafb9eb5f6b4e7fda86e31650c934
Kyle Driver <driver8684@gmail.com>
2021-04-05 20:19:40 -0400

    added email concrete and empty email test classes

---
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,19 @@
+//Programmer: Kyle Driver
+//Date: 3/21/2021
+
+package org.apache.commons.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import javax.mail.Session;
+import javax.mail.internet.MimeMultipart;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+
+}
